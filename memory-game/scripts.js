0531c64403537e052f66a5555980e3292efa6a96
rodriguezje96 <84817384+rodriguezje96@users.noreply.github.com>
2021-08-18 19:51:57 -0300

    Add files via upload

--- memory-game/scripts.js
+++ memory-game/scripts.js
@@ -1,7 +1,37 @@
-const cards = document.querySelectorAll ('.memory-card');
+const cards = document.querySelectorAll('.memory-card');
 
-function flipCard () {
-this.classList.toggle('flip');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        // primera carta
+        hasFlippedCard = true;
+        firstCard = this;
+    }
+    else {
+        // segunda carta
+        hasFlippedCard = false;
+        secondCard = this;
+        // matchean?
+        if (firstCard.dataset.framework === secondCard.dataset.framework) {
+            // matchearon
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+            console.log('matchearon');
+        }
+        else {
+            // no hay match
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1500);
+
+        }
+
+    }
 }
 
-cards.forEach(card => card.addEventListener ('click', flipCard));
+cards.forEach(card => card.addEventListener('click', flipCard));
